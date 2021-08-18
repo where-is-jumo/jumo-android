94cc697dad4299909d21ddeabd27590be2b79568
JaeHeung Choi <pish11010@gmail.com>
2021-08-18 23:09:05 +0900

    glide image view binding adapters

---
+++ jumo-android-boilerplate/src/main/java/com/jumo/boilerplate/ImageViewBindingAdapters.kt
@@ -0,0 +1,23 @@
+package com.jumo.boilerplate
+
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.signature.ObjectKey
+
+object ImageViewBindingAdapters {
+  @BindingAdapter("app:glide_imageUrl", "app:glide_placeholder", requireAll = false)
+  @JvmStatic
+  fun ImageView.loadImage(url: String?, placeholder: Drawable?) {
+    Glide.with(this.context)
+      .load(url)
+      .placeholder(placeholder)
+      .signature(ObjectKey("${url}_${placeholder}"))
+      .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+      .apply(RequestOptions().fitCenter())
+      .into(this)
+  }
+}
