04ff03f1fdd71cc6a7fa95496cf4a1e2594700a4
JaeHeung Choi <pish11010@gmail.com>
2021-08-22 16:18:51 +0900

    using passed data arg title

--- app/src/main/java/com/jumo/jumoexample/ui/NavTest2Fragment.kt
+++ app/src/main/java/com/jumo/jumoexample/ui/NavTest2Fragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.navArgs
 import com.jumo.jumoexample.R
 
 class NavTest2Fragment : Fragment() {
+  private val arg: NavTest2FragmentArgs by navArgs()
+
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?,
   ): View? {
     return inflater.inflate(R.layout.fragment_nav_test2, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    view.findViewById<TextView>(R.id.viewText).text = arg.title
   }
 }
