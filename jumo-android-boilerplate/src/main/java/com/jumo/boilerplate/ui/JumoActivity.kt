3db6986f2024e4111b07869717e3fc45b3444d9a
JaeHeung Choi <pish11010@gmail.com>
2021-08-29 19:08:24 +0900

    add appBarConfig and bottomNavigation

--- jumo-android-boilerplate/src/main/java/com/jumo/boilerplate/ui/JumoActivity.kt
+++ jumo-android-boilerplate/src/main/java/com/jumo/boilerplate/ui/JumoActivity.kt
@@ -7,11 +7,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.navigation.NavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.jumo.ktx.getNavHostFragment
+import com.jumo.ktx.gone
 
 open class JumoActivity<VDB : ViewDataBinding>(
   @LayoutRes private val layoutId: Int,
   @IdRes private val navControllerId: Int,
+  val mainDestinations: Set<Int>,
 ) : AppCompatActivity() {
   val binding: VDB by lazy {
     DataBindingUtil.setContentView<VDB>(this, layoutId)
@@ -21,8 +28,25 @@ open class JumoActivity<VDB : ViewDataBinding>(
     getNavHostFragment(navControllerId).navController
   }
 
+  private val appBarConfiguration by lazy {
+    AppBarConfiguration(mainDestinations)
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(binding.root)
+    setupActionBarWithNavController(navController, appBarConfiguration)
+
+  }
+
+  override fun onSupportNavigateUp(): Boolean {
+    return navController.navigateUp(appBarConfiguration)
+  }
+
+  fun setBottomNavigation(viewBottomNavigation: BottomNavigationView) {
+    viewBottomNavigation.setupWithNavController(navController)
+    navController.addOnDestinationChangedListener { _, destination, _ ->
+      viewBottomNavigation.gone(destination.id !in mainDestinations)
+    }
   }
 }
