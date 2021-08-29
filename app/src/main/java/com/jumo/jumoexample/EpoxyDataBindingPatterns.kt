4b5f91728ff87f743825372bef5387bb78079855
JaeHeung Choi <pish11010@gmail.com>
2021-08-29 15:56:13 +0900

    add epoxy data binding patterns

---
+++ app/src/main/java/com/jumo/jumoexample/EpoxyDataBindingPatterns.kt
@@ -0,0 +1,6 @@
+package com.jumo.jumoexample
+
+import com.airbnb.epoxy.EpoxyDataBindingPattern
+
+@EpoxyDataBindingPattern(rClass = R::class, layoutPrefix = "epoxy")
+object EpoxyDataBindingPatterns
