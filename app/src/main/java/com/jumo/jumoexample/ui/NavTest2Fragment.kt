cbf9c5d582377835289c2b5046682bd902532e40
heunghan <pish11010@gmail.com>
2021-08-29 16:48:06 +0900

    add epoxy example (#15)
    * add gradle for epoxy
    
    * add epoxy data binding patterns
    
    * create epoxy layout
    
    * using epoxy recycler view in fragment
    
    * add list

--- app/src/main/java/com/jumo/jumoexample/ui/NavTest2Fragment.kt
+++ app/src/main/java/com/jumo/jumoexample/ui/NavTest2Fragment.kt
@@ -7,7 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.navigation.fragment.navArgs
+import com.airbnb.epoxy.EpoxyRecyclerView
 import com.jumo.jumoexample.R
+import com.jumo.jumoexample.listItemExample
+import com.jumo.ktx.toast
 
 class NavTest2Fragment : Fragment() {
   private val arg: NavTest2FragmentArgs by navArgs()
@@ -21,6 +24,23 @@ class NavTest2Fragment : Fragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    view.findViewById<TextView>(R.id.viewText).text = arg.title
+    val epoxy = view.findViewById<EpoxyRecyclerView>(R.id.viewEpoxyList)
+    epoxy.withModels {
+      val list = arrayOf(
+        "고양이1" to "https://cdn2.thecatapi.com/images/YQKJJcqNZ.jpg",
+        "고양이2" to "https://cdn2.thecatapi.com/images/p46ys1bGF.jpg",
+        "고양이3" to "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg",
+      )
+      list.forEachIndexed { index, (title, url) ->
+        listItemExample {
+          id("item")
+          title(title)
+          imageUrl(url)
+          onClickItem { model, parentView, clickedView, position ->
+            toast("clicked $position item\n title is ${model.title()}")
+          }
+        }
+      }
+    }
   }
 }
