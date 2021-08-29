f6f2a4fa9207d30eba65531742d44ba62377dd21
JaeHeung Choi <pish11010@gmail.com>
2021-08-29 16:47:38 +0900

    add list

--- app/src/main/java/com/jumo/jumoexample/ui/NavTest2Fragment.kt
+++ app/src/main/java/com/jumo/jumoexample/ui/NavTest2Fragment.kt
@@ -9,6 +9,8 @@ import android.widget.TextView
 import androidx.navigation.fragment.navArgs
 import com.airbnb.epoxy.EpoxyRecyclerView
 import com.jumo.jumoexample.R
+import com.jumo.jumoexample.listItemExample
+import com.jumo.ktx.toast
 
 class NavTest2Fragment : Fragment() {
   private val arg: NavTest2FragmentArgs by navArgs()
@@ -24,6 +26,21 @@ class NavTest2Fragment : Fragment() {
     super.onViewCreated(view, savedInstanceState)
     val epoxy = view.findViewById<EpoxyRecyclerView>(R.id.viewEpoxyList)
     epoxy.withModels {
+      val list = arrayOf(
+        "고양이1" to "https://cdn2.thecatapi.com/images/YQKJJcqNZ.jpg",
+        "고양이2" to "https://cdn2.thecatapi.com/images/p46ys1bGF.jpg",
+        "고양이3" to "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg",
+      )
+      list.forEachIndexed { index, (title, url) ->
+        listItemExample {
+          id("item")
+          title(title)
+          imageUrl(url)
+          onClickItem { model, parentView, clickedView, position ->
+            toast("clicked $position item\n title is ${model.title()}")
+          }
+        }
+      }
     }
   }
 }
