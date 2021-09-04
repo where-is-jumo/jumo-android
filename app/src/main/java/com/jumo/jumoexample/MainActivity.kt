094b16db4d6c078d887bd94f80d08432564e18e1
JaeHeung Choi <pish11010@gmail.com>
2021-09-04 19:25:48 +0900

    connect bottom navigtaion view with nav controller

--- app/src/main/java/com/jumo/jumoexample/MainActivity.kt
+++ app/src/main/java/com/jumo/jumoexample/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.jumo.jumoexample
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.jumo.ktx.gone
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +19,8 @@ class MainActivity : AppCompatActivity() {
     val navHostFragment =
       supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
     val navController = navHostFragment.navController
-    findViewById<BottomNavigationView>(R.id.viewBottomNav).setupWithNavController(navController)
+    val viewBottomNavigation = findViewById<BottomNavigationView>(R.id.viewBottomNav)
+    viewBottomNavigation.setupWithNavController(navController)
   }
 
 }
