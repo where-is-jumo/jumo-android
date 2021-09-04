5f4e33c185b7ea165a1906484d771683b2fba6e6
JaeHeung Choi <pish11010@gmail.com>
2021-09-04 19:14:47 +0900

    add actvitiy extension

--- jumo-android-ktx/src/main/java/com/jumo/ktx/ActivityExtension.kt
+++ jumo-android-ktx/src/main/java/com/jumo/ktx/ActivityExtension.kt
@@ -5,7 +5,10 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
+import androidx.annotation.IdRes
 import androidx.core.os.bundleOf
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.fragment.NavHostFragment
 
 fun Intent.launchActivity(context: Context) = context.startActivity(this)
 
@@ -15,3 +18,7 @@ inline fun <reified A : Activity> ActivityResultLauncher<Intent>.launchActivity(
 ) = this.launch(Intent(context, A::class.java).apply {
   putExtras(extra)
 })
+
+fun FragmentActivity.getNavHostFragment(@IdRes id: Int): NavHostFragment {
+  return supportFragmentManager.findFragmentById(id) as NavHostFragment
+}
