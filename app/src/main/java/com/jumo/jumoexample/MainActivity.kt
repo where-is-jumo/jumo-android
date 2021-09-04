e63b777090c0574bcbc73ab9be95c4967c507549
JaeHeung Choi <pish11010@gmail.com>
2021-09-04 19:43:39 +0900

    using JumoActivity

--- app/src/main/java/com/jumo/jumoexample/MainActivity.kt
+++ app/src/main/java/com/jumo/jumoexample/MainActivity.kt
@@ -6,34 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.jumo.boilerplate.ui.JumoActivity
+import com.jumo.jumoexample.databinding.ActivityMainBinding
 import com.jumo.ktx.gone
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : JumoActivity<ActivityMainBinding>(
+  layoutId = R.layout.activity_main,
+  navControllerId = R.id.nav_host_fragment,
+  mainDestinations = setOf(
+    R.id.navTestFragment,
+    R.id.navExample2Fragment,
+    R.id.navExample3Fragment,
+  )
+) {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     setTheme(R.style.AppTheme)
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-
-//    startActivity(Intent(this, OssLicensesMenuActivity::class.java))
-    val navHostFragment =
-      supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-    val navController = navHostFragment.navController
-    val viewBottomNavigation = findViewById<BottomNavigationView>(R.id.viewBottomNav)
-    viewBottomNavigation.setupWithNavController(navController)
-
-    val mainDestinations = setOf(
-      R.id.navTestFragment,
-      R.id.navExample2Fragment,
-      R.id.navExample3Fragment
-    )
-    navController.addOnDestinationChangedListener { _, destination, _ ->
-      viewBottomNavigation.visibility = when (destination.id) {
-        in mainDestinations -> View.VISIBLE
-        else -> View.GONE
-      }
-    }
+    setBottomNavigation(binding.viewBottomNav)
   }
 
 }
